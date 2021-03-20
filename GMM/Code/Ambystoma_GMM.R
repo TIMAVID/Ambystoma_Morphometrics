f13c66cb7074e3f57be22e62afca33de9662462e
David Ledesma <ledesma-david@utexas.edu>
2021-03-19 21:51:14 -0500

    cc

---
+++ GMM/Code/Ambystoma_GMM.R
@@ -0,0 +1,125 @@
+## Load in data ##
+
+download.file(   "https://github.com/TIMAVID/Ambystoma/blob/fe4c0fe3f83265c6aa656946b696096524cdd26d/GMM/Data/GMM_data.RData?raw=true",   "GMM_data.RData")
+load("GMM_data.RData")
+
+library(geomorph)
+
+
+# Generalized procrustes analysis
+
+GPA_landmarks_sub <- gpagen(GMM_data_sub$land)
+
+#Create geomorph data frame
+
+Amb_gdf<-geomorph.data.frame(coords=GPA_landmarks_sub$coords,
+                             size=GPA_landmarks_sub$Csize, species=GMM_data_sub$species)
+## PCA ##
+
+Amb_PCA2 <- gm.prcomp(GPA_landmarks_sub$coords)
+
+## ANOVA ##
+
+# Without size
+Amb_anova <- procD.lm(coords ~ species, 
+                      data = Amb_gdf, iter = 999, 
+                      RRPP = TRUE, print.progress = FALSE)
+Amb_anova$aov.table
+
+plot(Amb_anova, type = "diagnostics", outliers = TRUE)
+
+# With size
+Amb_anova_size <- procD.lm(coords ~ species*size, 
+                           data = Amb_gdf, iter = 999, 
+                           RRPP = TRUE, print.progress = FALSE)
+Amb_anova_size$aov.table
+
+plot(Amb_anova_size, type = "diagnostics", outliers = TRUE)
+
+#Post-hoc comparisons
+
+gp <-  interaction(Amb_gdf$species)
+PW <- pairwise(Amb_anova, groups = gp, covariate = NULL)
+
+summary(PW, test.type = "dist", confidence = 0.95, stat.table = TRUE)
+summary(PW, test.type = "dist", confidence = 0.95, stat.table = FALSE)
+
+## Phylogenetic signal ##
+
+# Load in data #
+require(phytools)
+
+download.file(   "https://github.com/TIMAVID/Ambystoma/blob/master/GMM/Data/Amb_species?raw=true",   "Amb_species.txt")
+
+# Read in tree
+
+tree <- read.newick("Amb_species.txt")
+
+par(mar=c(1,1,1,1))
+tree$tip.label<-gsub("^", "A.", tree$tip.label)
+plot(tree)
+
+#Subset tree to include only GMM species
+Amb_species<-unique(GMM_data_sub$species)
+tips<-tree$tip.label
+ii<-sapply(Amb_species,function(x,y) grep(x,y)[1],y=tips)
+tree<-drop.tip(tree,setdiff(tree$tip.label,tips[ii]))
+plotTree(tree,ftype="i")
+#Tree did not include A.mavortium so I lumped that species with A.tigrinum
+Amb_gdf$species<-gsub("A.mavortium", "A.tigrinum", Amb_gdf$species, fixed = TRUE)
+Amb_gdf$species<-as.factor(Amb_gdf$species)
+
+#Preformed a group PCA
+library(Morpho)
+gpca <- groupPCA(Amb_gdf$coords, Amb_gdf$species, rounds=0)
+plot(gpca$groupmeans)
+
+#Performed a Phylogenetic PCA based on group means
+phylo.PCA <- gm.prcomp(gpca$groupmeans, phy = tree)
+summary(phylo.PCA)
+
+A_species<-attributes(gpca$groupmeans) #access attributes names
+A_species<-(A_species$dimnames[[3]])
+A_species<-as_factor(A_species)
+
+#Plot phylogenetic PCA
+plot(phylo.PCA, phylo = TRUE, main = "phylo PCA", col=A_species)
+
+#3D plot of plylogenetic PCA
+plotdat<-phylo.PCA$x[,1:3]
+colnames(plotdat)<-c("","","")#prevent axis labels
+obj<-phytools::phylomorphospace3d(tree,plotdat, method="dynamic",
+                                  control=list(ftype="off",spin=FALSE, box=FALSE), cex.symbol=0.5)
+spheres3d(phylo.PCA$x[,1:3], color=palette()[A_species], r=0.01)
+bbox3d(color = c("white"),shininess=15, alpha=0.3,xat=c(10), xlab="x",yat=c(10), ylab="y",zat=c(10), zlab="z")
+text3d((phylo.PCA$x[,1:3]+0.005), texts = substr(A_species,1,6))
+
+#Test for phylogenetic signal, uses Blomberg’s K to test for strength and significance of phylogenetic signal.
+physignal(gpca$groupmeans, tree, print.progress = F)
+
+#Phylogenetic generalized least squares
+
+avg_gdf<-geomorph.data.frame(coords=gpca$groupmeans, species=A_species) #make new geomorph dataframe with group mean coords
+
+pgls<-procD.pgls(coords~species, phy=tree, data=avg_gdf, print.progress = F) #Phylogenetic generalized least squares
+pgls$aov.table
+
+#Compare evolutionary rates in different portions of the tree based on brownian motion
+
+names(avg_gdf$species)<-avg_gdf$species
+rate.comp<-compare.evol.rates(avg_gdf$coords, tree, gp=avg_gdf$species, print.progress = F)
+
+plot(rate.comp)
+rate.comp$sigma.d.gp
+rate.comp$pairwise.pvalue
+
+
+
+
+
+
+
+
+
+
+
